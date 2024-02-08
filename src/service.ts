51e8fee7efbf2e922252a4ffe020238bba100aa8
Hugo Arregui <hugo@decentraland.org>
2024-02-08 18:11:50 -0300

    metrics

--- src/service.ts
+++ src/service.ts
@@ -1,14 +1,64 @@
 import { Lifecycle } from '@well-known-components/interfaces'
 import { AppComponents, TestComponents, WsData } from './types'
+import { Registry } from "prom-client"
 
 let connectionCounter = 0
+
+export const CONFIG_PREFIX = "WKC_METRICS" as const
+export function _configKey(key: Uppercase<string>): string {
+  return `${CONFIG_PREFIX}_${key.toUpperCase().replace(/^(_*)/, "")}`
+}
 
 export async function main(program: Lifecycle.EntryPointParameters<AppComponents | TestComponents>) {
   const { components, startComponents } = program
 
-  const { server, config, logs } = components
+  const { server, config, logs, metrics } = components
 
   const commitHash = (await config.getString('COMMIT_HASH')) || 'unknown'
+
+
+  const metricsPath = (await components.config.getString(_configKey("PUBLIC_PATH"))) || "/metrics"
+  const bearerToken = await components.config.getString(_configKey("BEARER_TOKEN"))
+  const rotateMetrics = await components.config.getString(_configKey("RESET_AT_NIGHT")) == 'true'
+
+  function calculateNextReset() {
+    return new Date(new Date(new Date().toDateString()).getTime() + 86400000).getTime()
+  }
+
+  let nextReset: number = calculateNextReset()
+
+  server.app.get(metricsPath, async (res, req) : Promise<void> => {
+    const body = await (metrics as any as { registry: Registry}).registry.metrics()
+
+    if (bearerToken) {
+      const header = req.getHeader('authorization')
+      console.log(header, typeof header)
+      if (!header) {
+        res.writeStatus('401 Forbidden')
+        res.end()
+        return
+      }
+      const [_, value] = header.split(" ")
+      console.log("HERE", value, bearerToken)
+      if (value != bearerToken) {
+        res.writeStatus('401 Forbidden')
+        res.end()
+        return
+      }
+    }
+
+    // heavy-metric servers that run for long hours tend to generate precision problems
+    // and memory degradation for histograms if not cleared enough. this method
+    // resets the metrics once per day at 00.00UTC
+    if (rotateMetrics && Date.now() > nextReset) {
+      nextReset = calculateNextReset()
+      metrics.resetAll()
+    }
+
+    res.writeStatus('200 OK')
+    res.writeHeader('content-type', 'application/json')
+    res.end(body)
+  })
 
   server.app.get('/health/live', (res) => {
     res.writeStatus('200 OK')
@@ -50,9 +100,9 @@ export async function main(program: Lifecycle.EntryPointParameters<AppComponents
     }
   })
 
-  server.app.filter(async (res, count) => {
-    console.log(res, count)
-  })
+  // server.app.filter(async (res, count) => {
+  //   console.log(res, count)
+  // })
 
   // start ports: db, listeners, synchronizations, etc
   await startComponents()
